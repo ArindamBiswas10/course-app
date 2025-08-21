36b70352de7a3e60d4b5a57eb8f5ec81d1f8fb1a
ArindamBiswas10 <arindombiswas29@gmail.com>
2025-08-21 23:26:49 +0530

    Initialized project

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const user = require('./routes/user');
+const course = require('./routes/course');
+const app = express();
+
+// Signup, signin, purchase, courses,
+
+app.use('/api/v1/user', user);
+app.use('/api/v1/course', course);
+
+app.listen(3000, () => { console.log('Server is running on port 3000'); });
